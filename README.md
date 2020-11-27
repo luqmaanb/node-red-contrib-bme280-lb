260b56ef65cd41c7536029a34da28885bbb2ea49
luqmaanb <lb@xinabox.cc>
2020-11-27 23:41:48 +0200

    comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # node-red-contrib-bme280-lb
+
+This is a node-red wrapper for the bme280. It uses I2C as communicating protocol. Hardware know-how is assumed.
+
+## Installation
+
+This is not an official node.js module hence it will need to be installed from github.
+
+1. Navigate to the node-red root directory ```cd ~/.node-red``` on your linux platform
+2. Install the module ```npm install luqmaanb/node-red-contrib-bme280-lb```
+3. Stop node-red ```node-red-stop```
+4. Start node-red ```node-red-start```
+
+## Example Flow
+
+```json
+[{"id":"da2d366f.a12ba8","type":"tab","label":"Flow 5","disabled":false,"info":""},{"id":"a3143510.b32548","type":"bme280","z":"da2d366f.a12ba8","name":"","bus":"1","address":"0x76","topic":"bme280","x":520,"y":340,"wires":[["6f3f007b.02edb"]]},{"id":"6f3f007b.02edb","type":"debug","z":"da2d366f.a12ba8","name":"","active":true,"tosidebar":true,"console":false,"tostatus":false,"complete":"false","statusVal":"","statusType":"auto","x":730,"y":340,"wires":[]},{"id":"4e33cf3d.7edb7","type":"inject","z":"da2d366f.a12ba8","name":"","props":[{"p":"payload"},{"p":"topic","vt":"str"}],"repeat":"1","crontab":"","once":false,"onceDelay":0.1,"topic":"","payload":"","payloadType":"date","x":340,"y":340,"wires":[["a3143510.b32548"]]}]
+```
+
+## Usage
+
+1. Import the flow above into node-red
+
+2. Screenshot of nodes
+
+![image](extras/bme280Flow.png)
+
+3. Select the I2C address from the drop down menu. Raspberry Pi uses bus 1.
+
+![image](extras/bme280I2cConfig.png)
+
+4. Flow output
+
+![image](extras/bme280FlowOutput.png)
+
--- bme280Node.js
+++ bme280Node.js
@@ -1,8 +1,15 @@
 'use strict';
 
+/**
+ * Wraps the bme280 driver in node-red wrapper
+ * @param  {} RED node-red wrapper
+ */
 module.exports = function (RED) {
     const BME280 = require('bme280');
-
+    /**
+     * This function creates the node and corresponding to html file
+     * @param  {} n links variables from html files
+     */
     function bme280(n) {
         RED.nodes.createNode(this, n);
         var node = this;
@@ -22,43 +29,56 @@ module.exports = function (RED) {
 
         node.sensor = new BME280(i2cSettings);
 
+        /**
+         * Initialize bme280
+         */
         const sensorInit = () => {
             node.sensor.init().then(function () {
                 node.initialized = true;
-                node.status({ fill: "green", shape: "dot", text: "BME280 Initialized." });
+                node.status({ fill: "green", shape: "dot", text: "BME280 Initialized." }); // show green dot on node if successful
                 node.log("BME280 Initialized.");
             }).catch(function (err) {
-                node.status({ fill: "red", shape: "ring", text: "Initialization failed." });
+                node.status({ fill: "red", shape: "ring", text: "Initialization failed." }); // show red ring on node if unsuccessful
                 node.error("Initialization failed. ->" + err);
             });
         };
-
+        /**
+         * Get readings from bme280
+         * @param  {object} msg payload object containing the data
+         */
         const sensorReading = (msg) => {
           node.sensor.readSensorData()
             .then((data) => {
               msg.payload = data;
-              node.send(msg);
+              node.send(msg); // send the payload
               let temp = node.type + "[T :" + Math.round(data.temperature_C);
-              node.status({ fill: "green", shape: "dot", text: temp + "Â°C]" });
+              node.status({ fill: "green", shape: "dot", text: temp + "°C]" }); // display the temperature at the node
             })
             .catch((err) => {
-              node.status({ fill: "red", shape: "ring", text: "BME280 reading failed." });
+              node.status({ fill: "red", shape: "ring", text: "BME280 reading failed." }); // display error message at the node
               node.error("BME280 reading failed ->" + err);
             });
             return null;
         };
 
-        // Init
+        // initialize sensor
         sensorInit();
-        // trigger measure
+
+
+        /**
+         * @param  {} 'input'
+         * @param  {object} msg payload object containing data
+         */
         node.on('input', function (msg) {
             if (!node.initialized) {
-                //try to reinit node until no sensor is found
+                // repeat initialization if failed
                 sensorInit();
                 return null;
             }
             sensorReading(msg);
           });
     }
+
+    // register the node
     RED.nodes.registerType("bme280", bme280);
 };
